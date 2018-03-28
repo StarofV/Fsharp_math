4562cb6b096dd04600396a9524b67974cd7325be
DogeSec <utdoge@gmail.com>
2018-03-27 21:15:52 -0600

    Improved.

--- samples/volume-of-cylinder.fs
+++ samples/volume-of-cylinder.fs
@@ -7,5 +7,6 @@ open System
 
 let Calculate radius hight : float = 
   let pi = 3.14159
-  radius * radius * pi * hight // Formula "pi(r^2)*h"
-printfn "volume is: %g" (Calculate 25 50) // Please input floats. I.E, 5 would be 5.0.
+  let radius_squared = radius*radius
+  radius_squared * pi * hight // Formula: pi(r^2)*h
+printfn "volume is: %g" (Calculate 25.0 50.0) // Please input floats. I.E, 5 would be 5.0
