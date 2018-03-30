50f699be1814eb75538e201569b8c5510b5218f0
DogeSec <utdoge@gmail.com>
2018-03-29 22:45:56 -0600

    the ": float" was not needed. Lol

--- real_world/Salary-based-on-2-factors.fs
+++ real_world/Salary-based-on-2-factors.fs
@@ -7,11 +7,11 @@ open System
 type factors(salary:float, monthly_bills:float) =
   member this.Salary = salary
   member this.Monthly_Bills = monthly_bills
-let instance = new factors(100000.0, 5000.0) (*The factors. Change them to yours. (make sure they are float)*
+let instance = new factors(100000.0, 5000.0) (*The factors. Change them to yours. (make sure they are float*)
 
 (*Subtract Salary from (monthly bills*12)*)
-let Subtract x y : float =
+let Subtract x y =
   x - y*12.0
 
 let final_result = Subtract instance.Salary instance.Monthly_Bills // Pass stuff to "Subtract" function
-Console.WriteLine("You make ${0} per year.", final_result) // Output the final_result
+Console.WriteLine("You make ${0} per year.", final_result) // Output the final_result.
